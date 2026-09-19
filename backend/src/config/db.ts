afe43c6d3794254c7eb8a5cafe0bdf30f50d1c7c
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-19 20:23:40 +0530

    runs backend on ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
